ccabf83a7c632d67b7416f236e6f25cbbad1b439
derekgeng15 <dxiageng@gmail.com>
2022-11-21 13:38:28 -0500

    Co-authored-by: emluo555 <emluo555@users.noreply.github.com>

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -1,92 +1,220 @@
-import edu.princeton.cs.algs4.IndexMinPQ;
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StopwatchCPU;
 
 public class SeamCarver {
 
-    private Picture picture;
-    private double[][] energy;
-    private IndexMinPQ<Double> pq; 
-    private double[] distTo;  
-    private int[] pathTo;   
+    private Picture picture; // stores picture
+    private int width, height; // dimension variables
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
-        this.picture = picture;
-        energy = new double[width()][height()];
-        for(int r = 0; r < width(); r++)
-            for(int c = 0; c < height(); c++) 
-                energy[r][c] = compEnergy(r,c); 
+        if (picture == null)
+            throw new IllegalArgumentException("picture cannot be null");
+        this.picture = new Picture(picture); // defensive copy
+        this.width = picture.width();
+        this.height = picture.height();
     }
- 
+
     // current picture
     public Picture picture() {
-        return picture;
+        return new Picture(picture);
     }
- 
+
     // width of current picture
     public int width() {
-        return picture.width();
+        return width;
     }
- 
+
     // height of current picture
     public int height() {
-        return picture.height();
+        return height;
     }
 
+    // energy (col, row)
     public double energy(int x, int y) {
-        return energy[x][y];
+        if (x >= width || y >= height || x < 0 || y < 0) {
+            throw new IllegalArgumentException("x or y coord out of bounds");
+        }
+        return compEnergy(x, y);
     }
- 
+
     // energy of pixel at column x and row y
     private double compEnergy(int x, int y) {
-        int up = picture.getRGB(x, (y + picture.height() - 1) % picture.height());
-        int down = picture.getRGB(x, (y + 1) % picture.height());
-        int left = picture.getRGB((x + picture.width() - 1) % picture.width(), y);
-        int right = picture.getRGB((x + 1) % picture.width(), y);
+        // get the RGB values for the adjacent pixels
+        int up = picture.getRGB(x, (y + height - 1) % height);
+        int down = picture.getRGB(x, (y + 1) % height);
+        int left = picture.getRGB((x + width - 1) % width, y);
+        int right = picture.getRGB((x + 1) % width, y);
         
-        int dx = delta(left, right);
-        int dy = delta(up, down);
-
-        return Math.sqrt(Math.pow(dx, 2)+ Math.pow(dy, 2));
+        // compute energy
+        return Math.sqrt(delta(left, right) + delta(up, down));
     }
 
-
-    private int delta(int x, int y) {
-        return Math.abs(x >> 16 - y >> 16) + Math.abs(x >> 8 - y >> 8) + Math.abs(x >> 0 - y >> 0);
+    // compute differences in RGB values
+    private double delta(int x, int y) {
+        return Math.pow(((x >> 16) & 0xFF) - ((y >> 16) & 0xFF), 2)  // delta R values
+                + Math.pow(((x >> 8) & 0xFF) - ((y >> 8) & 0xFF), 2) // detla G values
+                + Math.pow((x & 0xFF) - (y & 0xFF), 2); // delta B values
     }
-   
- 
+
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        int size = 3* (height() - 1) + 1;
-        pq = new IndexMinPQ<Double>(size);
-        distTo = new double[size];
-        pathTo = new int[size];
-
+        // transpose & compute energy of pixels
+        double[][] transpose = new double[height][width]; 
+        for (int r = 0; r < height; r++)
+            for (int c = 0; c < width; c++)
+                transpose[r][c] = compEnergy(c, r);
+        return findSeam(transpose, height, width);
     }
 
-    private 
- 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        
-        
+        // compute energy of pixels
+        double[][] energy = new double[width][height];
+        for (int r = 0; r < height; r++)
+            for (int c = 0; c < width; c++)
+                energy[c][r] = compEnergy(c, r);
+        return findSeam(energy, width, height);
     }
- 
+    
+    // finds the minimum energy seam
+    private int[] findSeam(double[][] e, int w, int h) {
+        int[][] parent = new int[w][h]; // store col from previous row
+        double[][] mem = new double[w][2]; // stores cummulative energy
+        
+        // stores energies of the first row (base case)
+        for (int c = 0; c < w; c++)
+            mem[c][0] = e[c][0];
+
+        // determine best path for each subsequent row
+        for (int r = 1; r < h; r++) {
+            for (int c = 0; c < w; c++) {
+                // default set path to pixel above it
+                mem[c][1] = mem[c][0];
+                parent[c][r] = c; 
+                
+                // compare top left pixel
+                if (c - 1 >= 0 && mem[c - 1][0] < mem[c][1]) {
+                    mem[c][1] = mem[c - 1][0];
+                    parent[c][r] = c - 1;
+                }
+
+                // compare top left pixel
+                if (c + 1 < w && mem[c + 1][0] < mem[c][1]) {
+                    mem[c][1] = mem[c + 1][0];
+                    parent[c][r] = c + 1;
+                }
+                mem[c][1] += e[c][r]; // add energy of pixel
+            }
+            // reset memory array
+            for (int c = 0; c < w; c++)
+                mem[c][0] = mem[c][1];
+        }
+        // find path with least energy
+        int curr = 0;
+        for (int c = 0; c < w; c++) {
+            if (mem[c][0] < mem[curr][0]) 
+                curr = c;
+        }
+        
+        // trace path with least energy
+        int[] out = new int[h]; // stores min path
+        for (int r = h - 1; r >= 0; r--) {
+            out[r] = curr;
+            curr = parent[curr][r];
+        }
+        return out;
+    }
+
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
+        if (seam == null)
+            throw new IllegalArgumentException("seam cannot be null");
+        if (seam.length != width)
+            throw new IllegalArgumentException("seam is wrong length");
+        for (int s : seam)
+            if (s < 0 || s >= height)
+                throw new IllegalArgumentException("element in seam is out of bounds");
+        for (int i = 1; i < seam.length; i++) 
+            if (Math.abs(seam[i] - seam[i - 1]) > 1)
+                throw new IllegalArgumentException("seam path is invalid");
         
+        // create updated picture, skip pixels in the removed seam
+        Picture newPic = new Picture(width, height - 1);
+        for (int c = 0; c < width; c++) {
+            int curr = 0;
+            for (int r = 0; r < height; r++)
+                if (r != seam[c])
+                    newPic.setRGB(c, curr++, picture.getRGB(c, r));
+        }
+        height--;
+        picture = newPic;
     }
- 
+
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
-        
+        if (seam == null)
+            throw new IllegalArgumentException("seam cannot be null");
+        if (seam.length != height)
+            throw new IllegalArgumentException("seam is wrong length");
+        for (int s : seam)
+            if (s < 0 || s >= width)
+                throw new IllegalArgumentException("element in seam is out of bounds");
+        for (int i = 1; i < seam.length; i++) 
+            if (Math.abs(seam[i] - seam[i - 1]) > 1)
+                throw new IllegalArgumentException("seam path is invalid");
+        // create updated picture, skip pixels in the removed seam
+        Picture newPic = new Picture(width - 1, height);
+        for (int r = 0; r < height; r++) {
+            int curr = 0;
+            for (int c = 0; c < width; c++)
+                if (c != seam[r])
+                    newPic.setRGB(curr++, r, picture.getRGB(c, r));
+        }
+        width--;
+        picture = newPic;
     }
- 
-    //  unit testing (required)
+
+    // unit testing (required)
     public static void main(String[] args) {
-        
+        Picture pic = new Picture("6x5.png");
+        SeamCarver sc = new SeamCarver(pic);
+        StdOut.println("Height: " + sc.height());
+        StdOut.println("Width: " + sc.width());
+        // test computing energy
+        StdOut.println("Energy of each pixel");
+        for (int r = 0; r < sc.height(); r++) {
+            for (int c = 0; c < sc.width(); c++)
+                StdOut.printf("%.2f\t", sc.energy(c, r));
+            StdOut.println();
+        }
+        // test finding & removing vert seam
+        int[] vs = sc.findVerticalSeam();
+        // output removed seam
+        StdOut.println("Vertical Seam of Least Energy");
+        for (int i : vs)
+            StdOut.print(i + " ");
+        StdOut.println();
+        sc.removeVerticalSeam(vs);
+        // test finding and removing horizontal seam
+        int[] hs = sc.findHorizontalSeam();
+        StdOut.println("Horizontal Seam of Least Energy");
+        for (int i : hs) 
+            StdOut.print(i + " ");
+        StdOut.println();
+        sc.removeHorizontalSeam(hs);
+        // return the new picture
+        sc.picture().save("out.png");
+        // timing test
+        for (String s : args) {
+            Picture stadium = new Picture(s);
+            SeamCarver test = new SeamCarver(stadium);
+            StopwatchCPU stopwatchCPU = new StopwatchCPU();
+            test.removeVerticalSeam(test.findVerticalSeam());
+            test.removeHorizontalSeam(test.findHorizontalSeam());
+            StdOut.println("Time: " + stopwatchCPU.elapsedTime());
+        }
     }
+
 }
- 
- 
--- seam/readme.txt
+++ seam/readme.txt
@@ -5,8 +5,9 @@ Programming Assignment 7: Seam Carving
  *  Describe concisely your algorithm to find a horizontal (or vertical)
  *  seam.
  **************************************************************************** */
-
-
+We use a bottom-up dynamic programming approach. We figure out which path to 
+take for a specific pixel based on the energy of the adjacent pixels in the 
+row above it.
 
 /* *****************************************************************************
  *  Describe what makes an image suitable to the seam-carving approach
@@ -14,8 +15,11 @@ Programming Assignment 7: Seam Carving
  *  image, without introducing visual artifacts). Describe an image that
  *  would not work well.
  **************************************************************************** */
-
-
+An image suitable to the seam-carving approach is one that has a lot of pixels
+of similar color grouped together so those pixels can be removed 
+without sacrificing the original image. An image that wouldn't work well is 
+one that has a lot of different colored pixels next to each other, 
+so removing a seam would change a lot.  
 
 /* *****************************************************************************
  *  Perform computational experiments to estimate the running time to reduce
@@ -30,32 +34,32 @@ Programming Assignment 7: Seam Carving
  *  data point to around 30 seconds for the largest one.
  **************************************************************************** */
 
-(keep W constant)
+(keep W constant) we use the stadium pictures, time slowed down with -Xint
  W = 2000
- multiplicative factor (for H) =
+ multiplicative factor (for H) = 2
 
  H           time (seconds)      ratio       log ratio
 ------------------------------------------------------
-...
-...
-...
-...
-...
-...
+250          4.375              
+500          8.703               1.989          0.992
+1000         17.410              2.000          1.000
+2000         35.187              2.021          1.015
+4000         70.101              1.992          0.994
+8000         140.642             2.006          1.004
 
 
-(keep H constant)
+(keep H constant) we use the city pictures, time slowed down with -Xint
  H = 2000
- multiplicative factor (for W) =
+ multiplicative factor (for W) = 2   
 
  W           time (seconds)      ratio       log ratio
 ------------------------------------------------------
-...
-...
-...
-...
-...
-...
+250          4.467             
+500          8.832               1.977        0.983
+1000         17.570              1.989        0.992
+2000         35.169              2.002        1.001
+4000         71.289              2.027        1.019
+8000         142.302             1.996        0.997
 
 
 
@@ -71,14 +75,17 @@ Programming Assignment 7: Seam Carving
  *  and exponents of the leading term (but not lower-order terms).
  *  Round each coefficient and exponent to two significant digits.
  **************************************************************************** */
+In a google sheet, we plotted the log of the heights vs the log of the
+runtime and then had it calculate the linear best-fit line, which had a 
+slope of 1.000. We did the same for the widths and found that the slope of 
+that best fit line was also 1.000. Multiplying the 1.000W x 1.000H = WH.
 
 
 Running time (in seconds) to find and remove one horizontal seam and one
 vertical seam, as a function of both W and H:
 
 
-    ~ 
-       _______________________________________
+    ~   1.000WH
 
 
 
@@ -99,14 +106,18 @@ vertical seam, as a function of both W and H:
 /* *****************************************************************************
  *  Describe any serious problems you encountered.                    
  **************************************************************************** */
-
+Our main problem was with the memory constraint. Originally we were storing 
+energy in a double matrix globally, but we changed it to be a local variable.
 
 /* *****************************************************************************
  *  If you worked with a partner, assert below that you followed
  *  the protocol as described on the assignment page. Give one
  *  sentence explaining what each of you contributed.
  **************************************************************************** */
+We followed the partner protocol as described on the assignment page. 
 
+Derek Geng: optimized the program for memory, fixed energy function bugs.  
+Emily Luo: developed DP approach to find seam, worked on energy function.  
 
 /* *****************************************************************************
  *  List any other comments here. Feel free to provide any feedback   

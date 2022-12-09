6a3c1962d554abd929dcd4af7613e2df7e106383
derekgeng15 <dxiageng@gmail.com>
2022-12-09 17:00:45 -0500

    fixed movetofront

--- burrows/MoveToFront.java
+++ burrows/MoveToFront.java
@@ -7,23 +7,18 @@ public class MoveToFront {
     // apply move-to-front encoding, reading from stdin and writing to stdout
     public static void encode() {
         char[] moveToFront = new char[256];
-        for(int i = 0; i < moveToFront.length; i++) {
+        for(int i = 0; i < moveToFront.length; i++)
             moveToFront[i] = (char) i;
-            //BinaryStdOut.write((char)i);
-        }
-        //BinaryStdOut.close();
         while (!BinaryStdIn.isEmpty()) {
-            char c = BinaryStdIn.readChar();
+            char c = BinaryStdIn.readChar(8);
             for (char i = 0; i < moveToFront.length; i++) {
                 if (c == moveToFront[i]) { 
                     BinaryStdOut.write(i);
-                    //StdOut.print(i);
                     for (int j = i; j > 0; j--)
                         moveToFront[j] = moveToFront[j - 1];
                     moveToFront[0] = c; 
                     break;
                 }
-                
             }
         }
         BinaryStdOut.close();
@@ -35,26 +30,22 @@ public class MoveToFront {
         for (int i = 0; i < moveToFront.length; i++)
             moveToFront[i] = (char) i;
         while (!BinaryStdIn.isEmpty()) {
-            int c = (int) BinaryStdIn.readChar();
+            char c = BinaryStdIn.readChar(8);
             BinaryStdOut.write(moveToFront[c]);
-            for (int j = c; j > 0; j--)
+            char temp = moveToFront[c];
+            for (char j = c; j > 0; j--)
                 moveToFront[j] = moveToFront[j - 1];
-            moveToFront[0] = (char) c; 
+            moveToFront[0] = temp; 
         }
+        BinaryStdOut.flush();
         BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
     public static void main(String[] args) {
-        //StdOut.println("here");
-        if (args[0].equals("-"))
-        {
-            //StdOut.println("-here");
-            encode();
-        }
-        else if (args[0].equals("+"))
-            decode();
+        if (args[0].equals("-")) encode();
+        else if (args[0].equals("+")) decode();
         
     }
 }

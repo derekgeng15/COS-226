05851025b1c36d1728e254a018b16d91ffdff56a
derekgeng15 <dxiageng@gmail.com>
2022-12-14 12:43:50 -0500

    Co-authored-by: emluo555 <emluo555@users.noreply.github.com>

--- burrows/BurrowsWheeler.java
+++ burrows/BurrowsWheeler.java
@@ -2,13 +2,10 @@ import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 import edu.princeton.cs.algs4.LSD;
 import edu.princeton.cs.algs4.SeparateChainingHashST;
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.Quick3string;
-import edu.princeton.cs.algs4.Quick3way;
 
 public class BurrowsWheeler {
-    private static final int R = 256; // alphabet size
+    private static final int R = 256; // ascii size
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output 
@@ -19,37 +16,35 @@ public class BurrowsWheeler {
         int first = 0;
         int size = cs.length();
         // construct the last char column of sorted suffix array
-        char c[] = new char[size]; 
-        // 
+        char[] c = new char[size]; 
         for (int i = 0; i < size; i++) {
-            int j = cs.index(i);
-            if (j != 0) 
-                c[i] = s.charAt(j - 1);
-            else {      
-                first = i;  // starting index found
-                c[i] = s.charAt(size - 1); 
-            }
+            int start = cs.index(i); // starting index of ith suffix
+            if (start == 0) // save first index
+                first = i;
+            // store the last index of the string starting from j
+            c[i] = s.charAt((start + size - 1) % size);
         }
-        // print index of first and prints the char array 
+        // write out first suffix index
         BinaryStdOut.write(first);
+        // write out encoded message
         for (int i = 0; i < size; i++) 
             BinaryStdOut.write(c[i]);
-        BinaryStdOut.flush();
         BinaryStdOut.close();
     }
 
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
-        int first = BinaryStdIn.readInt();
-        String t = BinaryStdIn.readString();
+        int first = BinaryStdIn.readInt(); // starting suffix index
+        String t = BinaryStdIn.readString(); // encoded string
         // create int array from the chars of string t, sort using LSD
         int[] sorted = new int[t.length()];
         for (int i = 0; i < t.length(); i++)
             sorted[i] = t.charAt(i);
         LSD.sort(sorted);   
         // create hash map to store characters & the indexes where they occur
-        SeparateChainingHashST<Character, Queue<Integer>> freq = new SeparateChainingHashST<>();
+        SeparateChainingHashST<Character, Queue<Integer>> freq 
+            = new SeparateChainingHashST<>();
         for (char i = 0; i < R; i++)
             freq.put(i, new Queue<Integer>());
         // enqueue index where that char occurs
@@ -58,17 +53,16 @@ public class BurrowsWheeler {
         
         // construct next array
         int[] next = new int[t.length()];
+        for (int i = 0; i < t.length(); i++)
+            // dequeue earliest nonused index where char occurs in sorted array
+            next[i] = freq.get((char) sorted[i]).dequeue();
+
+        // rebuild original string using next[]
+        int curr = first; // start with first
         for (int i = 0; i < t.length(); i++) {
-            // dequeue index where char occurs in sorted array
-            next[i] = freq.get((char)sorted[i]).dequeue();
-        }
-        // rebuild original string using next[], starting with index = first
-        int curr = first;
-        for (int i = 0; i < t.length(); i++) {
-            BinaryStdOut.write((char)sorted[curr]);
+            BinaryStdOut.write((char) sorted[curr]);
             curr = next[curr];
         }
-        BinaryStdOut.flush();
         BinaryStdOut.close();
     }
         
@@ -77,10 +71,8 @@ public class BurrowsWheeler {
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
-        if (args[0].equals("-")) 
-            transform();
-        else if (args[0].equals("+"))
-            inverseTransform();
+        if (args[0].equals("-")) transform();
+        else if (args[0].equals("+")) inverseTransform();
     }
 
 }
